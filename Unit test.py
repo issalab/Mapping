162f4ae292d93df91e839c9ce71fb4def580e9c7
toosi <tahereh>
2018-09-19 21:24:10 -0400

    Mapping Synthetic data, plots in ipynb

--- Unit test.py
+++ Unit test.py
@@ -36,7 +36,8 @@ trainfraci = 0.8  # image trainfrac
 splitfract = 0.5  # trial splitfrac
 nfoldi = 5
 nfoldt = 5
-ni = 500
+ni = 300
+noisy_map = False
 
 Data_type = 'synthetic'  # 'synthetic'#'HvM'
 
@@ -113,18 +114,25 @@ elif Data_type == 'synthetic':
 
     nf = 168  # # of features
     nt = 50  # # of trials
-    sds = np.logspace(-3, 1, num=int(nf))  # np.arange(0.5, 10, 1)
-    noise_dist = 'HvM_normal'  # 'normal'  # 'poisson'  # 'normal'
+    various_unit_stds = True
+    Collinearity = False
+
+    if various_unit_stds:
+        sds = np.logspace(-1, 1, num=int(nf))  # np.arange(0.5, 10, 1)
+    else:
+        sds = np.ones((nf))*sd
+
+    print(sds)
+
+    noise_dist = 'normal' #'HvM_normal'  # 'normal'  # 'poisson'  # 'normal'
     D = np.zeros((ni, nf, nt))
 
     hf = h5py.File(resultdir+'HvM_stats.h5', 'r')
     mu = np.array(hf.get('mu')).T
-    sds = np.array(hf.get('sd')).T
+    # sds = np.array(hf.get('sd')).T
     hf.close()
 
     D_mean = mu[:ni, :nf]  # np.random.rand(ni, nf)  # model M: nf feat x ni images
-
-    Collinearity = False
 
     if Collinearity:
         collinearity_r = np.random.uniform(low=0.8, high=1, size=nf - 1)
@@ -142,8 +150,8 @@ elif Data_type == 'synthetic':
     for i in range(ni):
         if noise_dist == 'normal':
             n = np.random.rand()
-            n1 = np.array([np.random.normal(0, sdf + n, size=int(nt * splitfract)) for sdf in sds])
-            n2 = np.array([np.random.normal(0, sdf + n, size=int(nt * splitfract)) for sd in sds])
+            n1 = np.array([np.random.normal(0, sdf, size=int(nt * splitfract)) for sdf in sds])
+            n2 = np.array([np.random.normal(0, sdf, size=int(nt * splitfract)) for sdf in sds])
             noise1[i] = n1  # (n1 - n1.min()) / (n1.max() - n1.min())
             noise2[i] = n2  # (n2 - n2.min()) / (n2.max() - n2.min())
         elif noise_dist == 'poisson':
@@ -158,150 +166,158 @@ elif Data_type == 'synthetic':
             noise1[i] = n1  # (n1-n1.min())/(n1.max()-n1.min())
             noise2[i] = n2  # (n2-n2.min())/(n2.max()-n2.min())
 
-        D[:, :, :int(nt * splitfract)] = D[:, :, :int(nt * splitfract)] + noise1
-        D[:, :, int(nt * splitfract):] = D[:, :, int(nt * splitfract):] + noise2
+    D[:, :, :int(nt * splitfract)] = D[:, :, :int(nt * splitfract)] + noise1
+    D[:, :, int(nt * splitfract):] = D[:, :, int(nt * splitfract):] + noise2
 
     # to test  HvM as syntheic
     # hf = h5py.File(resultdir+'D.h5', 'r')
     # D = np.array(hf.get('D')).mean(2)
     # hf.close()
 
-
-
     # Create model features M = D*W from data
     A = np.random.rand(nf, nf)
-    M = np.matmul(D_mean, A)
-
+    if noisy_map:
+        M = np.matmul(D.mean(2), A)
+    else:
+        M = np.matmul(D_mean, A)  # D_mean
+# ------------------------------------------------------------
+# Regression
 # ------------------------------------------------------------
 # reg_method = 'ridge'#'OMP'# 'PLS'#'ridge
-report_sitefit = False
+
 
 spearman_brown = False
 corr_method_for_inv = 'pearson'  # 'spearman'
-for reg_method in ['ridge']:
-
-    # regularization parameters
-
-    if reg_method == 'PLS':
-        n_components = nf
-        reg_params = n_components
-        report_popfit = True
-
-    elif reg_method == 'ridge':
-        reg_params = []
-        report_popfit = True
-
-    elif reg_method == 'OMP':
-        reg_params = []
-        report_popfit = False
-        report_sitefit = True
-
-    Data_params = [ni, nf, nt, nfoldi, nfoldt, trainfraci,splitfract, noise_dist,  sds, Collinearity, corr_method_for_inv]
-
-    data_list = get_mappings_unit_test(M, D, Data_type, Data_params, reg_method, reg_params, spearman_brown, report_sitefit, report_popfit)
-
-    # ----------------
-    # Read data
-    # ----------------
-    # ni,nf,nt = 20,20,10
-    # reg_method,reg_params,Collinearity = 'ridge','[]',True
-
-    # ni,nf,nt = 20,20,10
-    # reg_method,reg_params,Collinearity = 'PLS',nf,True
-
-    # ni,nf,nt = 200,20,10
-    # reg_method,reg_params,Collinearity = 'ridge','[]',True
-
-    # ni,nf,nt = 200,20,10
-    # reg_method,reg_params,Collinearity = 'PLS',nf,True
-    #
-    #
-    #
-    # file = open(resultdir+'unit_test_%s_%s_ni%d_nf%d_nt%d_collinearity%s.pickle'%\
-    #                             (reg_method,reg_params,ni,nf,nt,Collinearity),'rb')
-    # data_list = pickle.load(file)
-    # file.close()
-    r12, r11, r22, r12_reg, r11_reg, r22_reg, r12_reg_sitfit, r11_reg_sitfit, r22_reg_sitfit = data_list
-    # ----------------------------------------------------------
-
-    if reg_method == 'ridge':
-        color = 'r'
-    elif reg_method == 'PLS':
-        color = 'gray'
-    elif reg_method == 'OMP':
-        color = 'blue'
-
-    fig = plt.figure(figsize=[12, 5])
-    ax0 = fig.add_axes([0.1, 0.11, 0.25, 0.70])
-    ax1 = fig.add_axes([0.4, 0.11, 0.25, 0.70])
-    ax2 = fig.add_axes([0.7, 0.11, 0.25, 0.70])
-
-    ax0.scatter(np.sqrt(r11.mean(1)*r22.mean(1)), r12.mean(1), color = 'k', label ='Nom. vs. Denom.')
-    ax0.scatter(r22.mean(1), r11.mean(1), color=color, label='LHS vs. RHS')
-    ax0.plot([0, 1], [0, 1], ls='--', color='gray')
-
-    if reg_method != 'OMP':
-        ax1.scatter(np.sqrt(r11_reg.mean(1)*r22_reg.mean(1)), r12_reg.mean(1), color='k',
-                    label='Nom. vs. Denom.')
-        ax1.scatter(r22_reg.mean(1), r11_reg.mean(1), color=color, label='LHS vs. RHS')
-        ax1.plot([0, 1], [0, 1], ls='--', color='gray')
-
-    ax2.scatter(np.sqrt(r11_reg_sitfit.mean(1)*r22_reg_sitfit.mean(1)), r12_reg_sitfit.mean(1),
-                color='k', marker='x', label='Nom. vs. Denom.')
-    ax2.scatter(r22_reg_sitfit.mean(1), r11_reg_sitfit.mean(1), color=color, marker='x',
-                label='LHS vs. RHS')
-    ax2.plot([0, 1], [0, 1], ls='--', color='gray')
-
-    for ax in [ax0, ax2]:
-
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
-
-        handles, labels = ax.get_legend_handles_labels()
-        ax.legend(handles[:2], labels[:2], loc='upper left')
-
-    ax0.set_ylabel('r(model,data)_est OR r(model,model)')
-    ax1.set_yticks([])
-    ax0.set_title('VE=%.2f, N=%.2f, L= %.2f, R=%0.2f'%(np.nanmedian(r12/np.sqrt(r11*r22)), np.median(r12),
-                                                       np.median(r11), np.median(r22)))
-    ax0.text(0.0, 0.75, 'inversion', fontsize=14)
-    ax0.text(0.0, 0.71, corr_method_for_inv+'corr', fontsize=14)
-
-    if reg_method != 'OMP':
-        ax1.set_xlabel('r(model,model),r(data,data))^0.5 OR r(data,data)')
-        ax1.spines['right'].set_visible(False)
-        ax1.spines['top'].set_visible(False)
-
-        ax1.yaxis.set_ticks_position('left')
-        ax1.xaxis.set_ticks_position('bottom')
-
-        handles, labels = ax1.get_legend_handles_labels()
-        ax1.legend(handles[:2], labels[:2], loc='upper left')
-        ax1.text(0.0, 0.75, 'regression:%s'%reg_method, fontsize=14)
-        ax1.set_title('VE=%.2f, N=%.2f, L= %.2f, R=%0.2f'%(np.nanmedian(r12_reg/np.sqrt(r11_reg*r22_reg)), np.median(r12_reg),                                                   np.median(r11_reg),np.median(r22_reg)))
-    else:
-        ax1.set_xticks([])
-        ax1.axis('off')
-
-        ax0.set_xlabel('r(model,model),r(data,data))^0.5 OR r(data,data)')
-        ax2.set_xlabel('r(model,model),r(data,data))^0.5 OR r(data,data)')
-
-    print('Pinv: VarExp=%.3f, Num =%.3f, LHS= %.3f, RHS=%0.3f'%(np.nanmedian(r12/np.sqrt(r11*r22)),np.median(r12), np.median(r11), np.median(r22)))
-    print('Reg: VarExp=%.3f, Num =%.3f, LHS= %.3f, RHS=%0.3f'%(np.nanmedian(r12_reg/np.sqrt(r11_reg*r22_reg)), np.median(r12_reg), np.median(r11_reg), np.median(r22_reg)))
-    ax0.text(0.25, 1.2, '#images = %d, #features = %d, #trials = %d, Collinearity = %s, %s noise'%(ni, nf, nt, Collinearity,noise_dist), fontsize=14)
-
-    ax2.set_title('VE=%.2f, N=%.2f, L= %.2f, R=%0.2f'%(np.nanmedian(r12_reg_sitfit/np.sqrt(r11_reg_sitfit*r22_reg_sitfit)),np.median(r12_reg_sitfit), np.median(r11_reg_sitfit),np.median(r22_reg_sitfit)))
-    ax2.text(0.0,0.75,'regression-sitefit', fontsize=14)
-    ax2.text(0.0,0.65,'%s'%reg_method, fontsize=14)
-
-    print('Reg-sitefit: VarExp=%.3f, Num =%.3f, LHS= %.3f, RHS=%0.3f'%(np.nanmedian(r12_reg_sitfit/np.sqrt(r11_reg_sitfit*r22_reg_sitfit)),np.median(r12_reg_sitfit), np.median(r11_reg_sitfit), np.median(r22_reg_sitfit)))
-
-    plt.show()
-    fig.savefig(resultdir+'unit_test_%s_ni%d_nf%d_nt%d_Collin%s_%s_SB%s_%s.png'%(reg_method, ni, nf, nt, Collinearity, noise_dist, spearman_brown, corr_method_for_inv), dpi=300)
-    fig.savefig(resultdir+'unit_test_%s_ni%d_nf%d_nt%d_Collin%s_%s_SB%s_%s.pdf'%(reg_method, ni, nf, nt, Collinearity, noise_dist, spearman_brown, corr_method_for_inv), dpi=300)
 
 
+# regularization parameters
 
+# if reg_method == 'PLS':
+#     n_components = nf
+#     reg_params = n_components
+#     report_popfit = True
+#
+# elif reg_method == 'ridge':
+#     reg_params = []
+#     report_popfit = True
+#
+# elif reg_method == 'OMP':
+#     reg_params = []
+#     report_popfit = False
+#     report_sitefit = True
+
+reg_methods = ['OMP', 'PLS', 'ridge']
+reg_params_list = [[], nf, []]
+report_popfit = [False, True, True]  # [False, True, True]
+report_sitefit = [True, True, True]
+
+Data_params = [ni, nf, nt, nfoldi, nfoldt, trainfraci, splitfract, noise_dist,  sds, Collinearity, various_unit_stds, corr_method_for_inv, noisy_map]
+
+data_list = get_mappings_unit_test(M, D, Data_type, Data_params, reg_methods, reg_params_list, spearman_brown, report_sitefit, report_popfit)
+
+# ----------------
+# Read data
+# ----------------
+# ni,nf,nt = 20,20,10
+# reg_method,reg_params,Collinearity = 'ridge','[]',True
+
+# ni,nf,nt = 20,20,10
+# reg_method,reg_params,Collinearity = 'PLS',nf,True
+
+# ni,nf,nt = 200,20,10
+# reg_method,reg_params,Collinearity = 'ridge','[]',True
+
+# ni,nf,nt = 200,20,10
+# reg_method,reg_params,Collinearity = 'PLS',nf,True
+#
+#
+#
+# file = open(resultdir+'unit_test_%s_%s_ni%d_nf%d_nt%d_collinearity%s.pickle'%\
+#                             (reg_method,reg_params,ni,nf,nt,Collinearity),'rb')
+# data_list = pickle.load(file)
+# file.close()
+# r12, r11, r22, regressions_results = data_list
+# r12, r11, r22, r12_reg, r11_reg, r22_reg, r12_reg_sitfit, r11_reg_sitfit, r22_reg_sitfit = data_list
+# # ----------------------------------------------------------
+#
+# if reg_method == 'ridge':
+#     color = 'r'
+# elif reg_method == 'PLS':
+#     color = 'gray'
+# elif reg_method == 'OMP':
+#     color = 'blue'
+#
+# fig = plt.figure(figsize=[12, 5])
+# ax0 = fig.add_axes([0.1, 0.11, 0.25, 0.70])
+# ax1 = fig.add_axes([0.4, 0.11, 0.25, 0.70])
+# ax2 = fig.add_axes([0.7, 0.11, 0.25, 0.70])
+#
+# ax0.scatter(np.sqrt(r11.mean(1)*r22.mean(1)), r12.mean(1), color = 'k', label ='Nom. vs. Denom.')
+# ax0.scatter(r22.mean(1), r11.mean(1), color=color, label='LHS vs. RHS')
+# ax0.plot([0, 1], [0, 1], ls='--', color='gray')
+#
+# if reg_method != 'OMP':
+#     ax1.scatter(np.sqrt(r11_reg.mean(1)*r22_reg.mean(1)), r12_reg.mean(1), color='k',
+#                 label='Nom. vs. Denom.')
+#     ax1.scatter(r22_reg.mean(1), r11_reg.mean(1), color=color, label='LHS vs. RHS')
+#     ax1.plot([0, 1], [0, 1], ls='--', color='gray')
+#
+# ax2.scatter(np.sqrt(r11_reg_sitfit.mean(1)*r22_reg_sitfit.mean(1)), r12_reg_sitfit.mean(1),
+#             color='k', marker='x', label='Nom. vs. Denom.')
+# ax2.scatter(r22_reg_sitfit.mean(1), r11_reg_sitfit.mean(1), color=color, marker='x',
+#             label='LHS vs. RHS')
+# ax2.plot([0, 1], [0, 1], ls='--', color='gray')
+#
+# for ax in [ax0, ax2]:
+#
+#     ax.spines['right'].set_visible(False)
+#     ax.spines['top'].set_visible(False)
+#
+#     ax.yaxis.set_ticks_position('left')
+#     ax.xaxis.set_ticks_position('bottom')
+#
+#     handles, labels = ax.get_legend_handles_labels()
+#     ax.legend(handles[:2], labels[:2], loc='upper left')
+#
+# ax0.set_ylabel('r(model,data)_est OR r(model,model)')
+# ax1.set_yticks([])
+# ax0.set_title('VE=%.2f, N=%.2f, L= %.2f, R=%0.2f'%(np.nanmedian(r12/np.sqrt(r11*r22)), np.median(r12),
+#                                                    np.median(r11), np.median(r22)))
+# ax0.text(0.0, 0.75, 'inversion', fontsize=14)
+# ax0.text(0.0, 0.71, corr_method_for_inv+'corr', fontsize=14)
+#
+# if reg_method != 'OMP':
+#     ax1.set_xlabel('r(model,model),r(data,data))^0.5 OR r(data,data)')
+#     ax1.spines['right'].set_visible(False)
+#     ax1.spines['top'].set_visible(False)
+#
+#     ax1.yaxis.set_ticks_position('left')
+#     ax1.xaxis.set_ticks_position('bottom')
+#
+#     handles, labels = ax1.get_legend_handles_labels()
+#     ax1.legend(handles[:2], labels[:2], loc='upper left')
+#     ax1.text(0.0, 0.75, 'regression:%s'%reg_method, fontsize=14)
+#     ax1.set_title('VE=%.2f, N=%.2f, L= %.2f, R=%0.2f'%(np.nanmedian(r12_reg/np.sqrt(r11_reg*r22_reg)), np.median(r12_reg),                                                   np.median(r11_reg),np.median(r22_reg)))
+# else:
+#     ax1.set_xticks([])
+#     ax1.axis('off')
+#
+#     ax0.set_xlabel('r(model,model),r(data,data))^0.5 OR r(data,data)')
+#     ax2.set_xlabel('r(model,model),r(data,data))^0.5 OR r(data,data)')
+#
+# print('Pinv: VarExp=%.3f, Num =%.3f, LHS= %.3f, RHS=%0.3f'%(np.nanmedian(r12/np.sqrt(r11*r22)),np.median(r12), np.median(r11), np.median(r22)))
+# print('Reg: VarExp=%.3f, Num =%.3f, LHS= %.3f, RHS=%0.3f'%(np.nanmedian(r12_reg/np.sqrt(r11_reg*r22_reg)), np.median(r12_reg), np.median(r11_reg), np.median(r22_reg)))
+# ax0.text(0.25, 1.2, '#images = %d, #features = %d, #trials = %d, Collinearity = %s, %s noise'%(ni, nf, nt, Collinearity,noise_dist), fontsize=14)
+#
+# ax2.set_title('VE=%.2f, N=%.2f, L= %.2f, R=%0.2f'%(np.nanmedian(r12_reg_sitfit/np.sqrt(r11_reg_sitfit*r22_reg_sitfit)),np.median(r12_reg_sitfit), np.median(r11_reg_sitfit),np.median(r22_reg_sitfit)))
+# ax2.text(0.0,0.75,'regression-sitefit', fontsize=14)
+# ax2.text(0.0,0.65,'%s'%reg_method, fontsize=14)
+#
+# print('Reg-sitefit: VarExp=%.3f, Num =%.3f, LHS= %.3f, RHS=%0.3f'%(np.nanmedian(r12_reg_sitfit/np.sqrt(r11_reg_sitfit*r22_reg_sitfit)),np.median(r12_reg_sitfit), np.median(r11_reg_sitfit), np.median(r22_reg_sitfit)))
+#
+# plt.show()
+# fig.savefig(resultdir+'unit_test_%s_ni%d_nf%d_nt%d_Collin%s_%s_SB%s_%s.png'%(reg_method, ni, nf, nt, Collinearity, noise_dist, spearman_brown, corr_method_for_inv), dpi=300)
+# fig.savefig(resultdir+'unit_test_%s_ni%d_nf%d_nt%d_Collin%s_%s_SB%s_%s.pdf'%(reg_method, ni, nf, nt, Collinearity, noise_dist, spearman_brown, corr_method_for_inv), dpi=300)
+#
+#
+#
--- get_mappings_unit_test.py
+++ get_mappings_unit_test.py
@@ -8,26 +8,27 @@ Mapping = Mapping()
 resultdir = '/home/tahereh/Documents/Research/Results/Mapping_unit_test/'
 
 
-def get_mappings_unit_test(M, D, Data_type, Data_params, reg_method, reg_params, spearman_brown, report_sitefit, report_popfit):
-    ni,nf,nt,nfoldi,nfoldt,trainfraci,splitfract, noise_dist, sds, Collinearity, corr_method_for_inv= Data_params
+def get_mappings_unit_test(M, D, Data_type, Data_params, reg_methods, reg_params_list, spearman_brown, report_sitefit, report_popfit):
+    ni,nf,nt,nfoldi,nfoldt,trainfraci,splitfract, noise_dist, sds, Collinearity,  various_unit_stds, corr_method_for_inv, noisy_map= Data_params
 
     r12 = np.zeros((nf, nfoldi, nfoldt))
     r11 = np.zeros((nf, nfoldi, nfoldt))
     r22 = np.zeros((nf, nfoldi, nfoldt))
 
-    r12_reg = np.zeros((nf, nfoldi, nfoldt))
-    r11_reg = np.zeros((nf, nfoldi, nfoldt))
-    r22_reg = np.zeros((nf, nfoldi, nfoldt))
+    r12_reg = np.zeros((nf, len(reg_methods), nfoldi, nfoldt))
+    r11_reg = np.zeros((nf, len(reg_methods), nfoldi, nfoldt))
+    r22_reg = np.zeros((nf, len(reg_methods), nfoldi, nfoldt))
 
-    r12_reg_sitfit = np.zeros((nf, nfoldi, nfoldt))
-    r11_reg_sitfit = np.zeros((nf, nfoldi, nfoldt))
-    r22_reg_sitfit = np.zeros((nf, nfoldi, nfoldt))
+    r12_reg_sitfit = np.zeros((nf, len(reg_methods), nfoldi, nfoldt))
+    r11_reg_sitfit = np.zeros((nf, len(reg_methods), nfoldi, nfoldt))
+    r22_reg_sitfit = np.zeros((nf, len(reg_methods), nfoldi, nfoldt))
 
     start = time.time()
     time_sitefit = []
     time_popfit = []
 
     print(time.time() - start, end="")
+    regressions_results = []
     for fi in range(nfoldi):
 
         # train/test image split
@@ -41,6 +42,7 @@ def get_mappings_unit_test(M, D, Data_type, Data_params, reg_method, reg_params,
             indices = np.random.permutation(nt)
             D1 = D[:, :, indices[:int(nt * splitfract)]].mean(2)
             D2 = D[:, :, indices[int(nt * splitfract):]].mean(2)
+
 
             # NUMERATOR: Fit on train, test on test
             Ahat = np.dot(np.linalg.pinv(M[indtraini, :]), D1[indtraini, :])
@@ -67,71 +69,76 @@ def get_mappings_unit_test(M, D, Data_type, Data_params, reg_method, reg_params,
                 r11[:, fi, ft] = [ss.spearmanr(lhs1[:, indf], lhs2[:, indf])[0] for indf in range(nf)]
 
             # Regression
-            train_inds, test_inds = indtraini, indtesti
-            model_features_X, half1, half2 = M, D1, D2
-            zscored_observations = False
-            return_fitted_reg = False
 
-            # population fit
-            if report_popfit:
-                start_popfit = time.time()
+            for r, reg_method in enumerate(reg_methods):
+                reg_params = reg_params_list[r]
 
-                if spearman_brown:
-                    _, r_Nom_sites = Mapping.Numerator(train_inds, test_inds, model_features_X, np.mean([half2, half2], axis=0), reg_method,
-                                                           reg_params, zscored_observations, return_fitted_reg)
-                else:
-                    _ , r_Nom_sites = Mapping.Numerator(train_inds, test_inds, model_features_X, half1, reg_method,
-                                                           reg_params, zscored_observations, return_fitted_reg)
+                train_inds, test_inds = indtraini, indtesti
+                model_features_X, half1, half2 = M, D1, D2
+                zscored_observations = False
+                return_fitted_reg = False
 
-                _, r_RHS_sites = Mapping.Denom_RHS(train_inds, test_inds, half1, half2)
-                _, r_LHS_sites = Mapping.Denom_LHS(train_inds, test_inds, model_features_X, half1, half2, reg_method, reg_params,
-                                                       zscored_observations, return_fitted_reg)
-                r12_reg[:, fi, ft] = r_Nom_sites
+                # population fit
+                if report_popfit[r]:
+                    start_popfit = time.time()
 
-                if spearman_brown:
-                    r_RHS_sites_sb = [Mapping.spearman_brown_correction(r) for r in r_RHS_sites]
-                    r_LHS_sites_sb = [Mapping.spearman_brown_correction(r) for r in r_LHS_sites]
-
-                    r22_reg[:, fi, ft] = r_RHS_sites_sb
-                    r11_reg[:, fi, ft] = r_LHS_sites_sb
-                else:
-                    r22_reg[:, fi, ft] = r_RHS_sites
-                    r11_reg[:, fi, ft] = r_LHS_sites
-                time_popfit.append([time.time() - start_popfit])
-
-
-
-            # site fit
-            if report_sitefit:
-                start_sitefit = time.time()
-                for n in range(nf):
-                    return_fitted_reg = False
-                    r_Nom, _ = Mapping.Numerator(train_inds, test_inds, model_features_X, half1[:, n], reg_method, reg_params,
-                                                 zscored_observations, return_fitted_reg)
-
-                    r_LHS, _ = Mapping.Denom_LHS(train_inds, test_inds, model_features_X, half1[:, n], half2[:, n], reg_method,
-                                                 reg_params, zscored_observations, return_fitted_reg)
-                    r12_reg_sitfit[n, fi, ft] = r_Nom
-
-                    r11_reg_sitfit[n, fi, ft] = r_LHS
-
-                    if report_popfit:
-                        r22_reg_sitfit[:, fi, ft] = r_RHS_sites
+                    if spearman_brown:
+                        _, r_Nom_sites = Mapping.Numerator(train_inds, test_inds, model_features_X, np.mean([half2, half2], axis=0), reg_method,
+                                                               reg_params, zscored_observations, return_fitted_reg)
                     else:
-                        r_RHS, _ = Mapping.Denom_RHS(train_inds, test_inds, half1[:, n], half2[:, n])
-                        r22_reg_sitfit[n, fi, ft] = r_RHS
-                time_sitefit.append([time.time() -start_sitefit])
+                        _ , r_Nom_sites = Mapping.Numerator(train_inds, test_inds, model_features_X, half1, reg_method,
+                                                               reg_params, zscored_observations, return_fitted_reg)
+
+                    _, r_RHS_sites = Mapping.Denom_RHS(train_inds, test_inds, half1, half2)
+                    _, r_LHS_sites = Mapping.Denom_LHS(train_inds, test_inds, model_features_X, half1, half2, reg_method, reg_params,
+                                                           zscored_observations, return_fitted_reg)
+                    r12_reg[:, r, fi, ft] = r_Nom_sites
+
+                    if spearman_brown:
+                        r_RHS_sites_sb = [Mapping.spearman_brown_correction(r) for r in r_RHS_sites]
+                        r_LHS_sites_sb = [Mapping.spearman_brown_correction(r) for r in r_LHS_sites]
+
+                        r22_reg[:, r,  fi, ft] = r_RHS_sites_sb
+                        r11_reg[:, r, fi, ft] = r_LHS_sites_sb
+                    else:
+                        r22_reg[:, r, fi, ft] = r_RHS_sites
+                        r11_reg[:, r, fi, ft] = r_LHS_sites
+                    time_popfit.append([time.time() - start_popfit])
+
+
+
+                # site fit
+                if report_sitefit[r]:
+                    start_sitefit = time.time()
+                    for n in range(nf):
+                        return_fitted_reg = False
+                        r_Nom, _ = Mapping.Numerator(train_inds, test_inds, model_features_X, half1[:, n], reg_method, reg_params,
+                                                     zscored_observations, return_fitted_reg)
+
+                        r_LHS, _ = Mapping.Denom_LHS(train_inds, test_inds, model_features_X, half1[:, n], half2[:, n], reg_method,
+                                                     reg_params, zscored_observations, return_fitted_reg)
+                        r12_reg_sitfit[n, r, fi, ft] = r_Nom
+
+                        r11_reg_sitfit[n, r, fi, ft] = r_LHS
+
+                        if report_popfit[r]:
+                            r22_reg_sitfit[:, r, fi, ft] = r_RHS_sites
+                        else:
+                            r_RHS, _ = Mapping.Denom_RHS(train_inds, test_inds, half1[:, n], half2[:, n])
+                            r22_reg_sitfit[n, r, fi, ft] = r_RHS
+                    time_sitefit.append([time.time() -start_sitefit])
 
     print('popfit for %s took %.2f seconds' %(reg_method, np.mean(time_popfit)))
     print('sitefit for %s took %.2f seconds' %(reg_method, np.mean(time_sitefit)))
 
     r12, r11, r22 = np.mean(r12, 2), np.mean(r11, 2), np.mean(r22, 2)
-    r12_reg, r11_reg, r22_reg = np.mean(r12_reg, 2), np.mean(r11_reg, 2), np.mean(r22_reg, 2)
-    r12_reg_sitfit, r11_reg_sitfit, r22_reg_sitfit = np.mean(r12_reg_sitfit, 2), np.mean(r11_reg_sitfit, 2), np.mean(
-        r22_reg_sitfit, 2)
+    r12_reg, r11_reg, r22_reg = np.mean(r12_reg.mean(3), 2), np.mean(r11_reg.mean(3), 2), np.mean(r22_reg.mean(3), 2)
+    r12_reg_sitfit, r11_reg_sitfit, r22_reg_sitfit = np.mean(r12_reg_sitfit.mean(3), 2), np.mean(r11_reg_sitfit.mean(3), 2), np.mean(
+        r22_reg_sitfit.mean(3), 2)
 
-    data_list = [r12, r11, r22, r12_reg, r11_reg, r22_reg, r12_reg_sitfit, r11_reg_sitfit, r22_reg_sitfit]
-    pickle.dump(data_list, open(resultdir + 'unit_test_%s_%s_ni%d_nf%d_nt%d_collinearity%s_%s_SB%s.pickle' % (
-    reg_method, reg_params, ni, nf, nt, Collinearity,noise_dist,spearman_brown), 'wb'))
+    data_list = [r12,r11,r22,r12_reg,r11_reg,r22_reg,r12_reg_sitfit,r11_reg_sitfit,r22_reg_sitfit]
+    pickle.dump(data_list, open(resultdir + 'unit_test_%s_ni%d_nf%d_nt%d_collinearity%s_%s_SB%s_VariousSd%s_noisymap%s.pickle' % (
+    reg_methods, ni, nf, nt, Collinearity, noise_dist, spearman_brown, various_unit_stds, noisy_map), 'wb'))
 
     return data_list
+
